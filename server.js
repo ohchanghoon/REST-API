877d8a169dc51be2835dfb384c3e993f44f3b831
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-07-07 17:08:38 +0900

    ID 파라미터를 이용해 User의 데이터를 조회하는 기능 추가

--- server.js
+++ server.js
@@ -21,10 +21,20 @@ server.get('/api/user', (req, res) => {
   res.json(users);
 });
 
+server.get('/api/user/:id', (req, res) => {
+  const user = users.find((u) => {
+    return u.id === req.params.id;
+  });
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ errorMessage: 'User was not found' });
+  }
+});
+
 server.post('/api/user', (req, res) => {
   users.push(req.body);
   res.json(users);
-  console.log(users);
 });
 
 server.listen(PORT, () => {
